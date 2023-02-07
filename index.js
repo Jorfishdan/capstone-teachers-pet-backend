da084417300867d51fbfbcb3dde42ad027f6cebe
Jorfishdan <jordan_fish@ymail.com>
2023-02-07 15:40:58 -0800

    installed cors and dotenv

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require('path')
+const cors = require("cors");
+require('dotenv').config()
+const videoRoutes = require("./routes/videos");
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/videos", videoRoutes);
+app.use(express.static('public'))
+
+app.listen(8080, function () {
+  console.log("Hello World");
+});
