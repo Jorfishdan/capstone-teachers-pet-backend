0c6bd837007feb7518e7a2228a7f90dc21204e57
Jorfishdan <jordan_fish@ymail.com>
2023-02-07 19:17:49 -0800

    added post route

--- routes/facts.js
+++ routes/facts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const facts = require("../data/facts.json");
 const { v4:uuid } = require("uuid");
 const fs = require("fs");
+const { executionAsyncId } = require("async_hooks");
 
 router.get("/", (req, res) => {
     const factGallery = factList();
@@ -15,10 +16,28 @@ router.get("/", (req, res) => {
         question: info.question,
         correct_answer: info.correct_answer,
         incorrect_answer:info.incorrect_answer,
+        explanation: info.explanation,
       };
     });
     res.send(displayFact);
   });
+
+  router.post("/", (req,res) => {
+    const {question, correct_answer, incorrect_answer, explanation} = req.body;
+    const newFact ={
+        id: uuid(),
+        category: "misc",
+        type: Boolean,
+        difficulty: "easy",
+        question, 
+        correct_answer,
+        explanation,
+    };
+    facts.push(newFact);
+    res.json(newFact)
+    fs.writeFileSync("./data/facts.json", JSON.stringify(facts))
+
+  })
 
   function factList() {
     const factPreview = fs.readFileSync("./data/facts.json");
