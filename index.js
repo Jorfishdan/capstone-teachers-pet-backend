6003bf987d16619eb28bd3190cb926268aa3a1ff
Jorfishdan <jordan_fish@ymail.com>
2023-02-07 16:38:41 -0800

    it reads my JSON file

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path')
 const cors = require("cors");
 require('dotenv').config()
-// const videoRoutes = require("./routes/videos");
+const factsRoutes = require("./routes/facts");
 
 
 app.use(cors());
 app.use(express.json());
 
-// app.use("/videos", videoRoutes);
+app.use("/facts", factsRoutes);
 // app.use(express.static('public'))
 
 app.listen(8080, function () {
--- routes/facts.js
+++ routes/facts.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const videos = require("../data/videos.json");
+const facts = require("../data/facts.json");
 const { v4:uuid } = require("uuid");
 const fs = require("fs");
 
 router.get("/", (req, res) => {
-    const videoGallery = videoList();
-    const displayVideo = videoGallery.map((vid) => {
+    const factGallery = factList();
+    const displayFact = factGallery.map((info) => {
       return {
-        id: vid.id,
-        title: vid.title,
-        channel: vid.channel,
-        image: vid.image,
+        id: info.id,
+        category: info.category,
+        type: info.type,
+        difficulty: info.difficulty,
+        question: info.question,
+        correct_answer: info.correct_answer,
+        incorrect_answer:info.incorrect_answer,
       };
     });
-    res.send(displayVideo);
+    res.send(displayFact);
   });
+
+  function factList() {
+    const factPreview = fs.readFileSync("./data/facts.json");
+    return JSON.parse(factPreview);
+  }
   
-  router.get("/:id", (req, res) => {
-    const videoMain = videoList();
-    const heroVideo = videoMain.find((mainVid) => {
-      return mainVid.id === req.params.id
-    });
-    res.send(heroVideo);
-  });
+//   router.get("/:id", (req, res) => {
+//     const videoMain = videoList();
+//     const heroVideo = videoMain.find((mainVid) => {
+//       return mainVid.id === req.params.id
+//     });
+//     res.send(heroVideo);
+//   });
+
+module.exports = router;
